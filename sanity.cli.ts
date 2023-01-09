61ceda75f59ab9f3d812eccbb84f5f462c94d467
Can Kanbur <cankanbur1@gmail.com>
2023-01-09 14:11:11 +0300

    added studio folder for sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,9 @@
-import {defineCliConfig} from 'sanity/cli'
-
+import { defineCliConfig } from 'sanity/cli';
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export default defineCliConfig({
   api: {
-    projectId: 'o6feidxe',
-    dataset: 'production'
-  }
-})
+    projectId,
+    dataset,
+  },
+});
--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
-  name: 'Content Studio',
-  title: 'nextjs blog',
+  name: 'Content_Studio',
+  title: 'Content Studio',
   basePath: '/studio',
   projectId,
   dataset,
